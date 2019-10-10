f4514337683247dca3df2d343e4e5a47a10d4bb3
andrew levun <andrewlevun@gmail.com>
2019-10-10 14:33:25 +0300

    upload project 101019

---
+++ js/js.js
@@ -0,0 +1,18 @@
+
+   $('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1
+       /*centerMode: true,
+       ceterPadding: "25px",
+       /*dots:true,
+       fade: true*/
+
+   });
+
+   $('body').flipLightBox
+   ({ lightbox_flip_speed: 500, 
+      lightbox_border_color: '#666666' 
+   })
+
+   
