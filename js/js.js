f105a4ad2655e1f6112220cb891b70003da19b01
andrew levun <andrewlevun@gmail.com>
2019-10-15 12:06:25 +0300

    up

--- js/js.js
+++ js/js.js
@@ -3,11 +3,8 @@
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1
-       /*centerMode: true,
-       ceterPadding: "25px",
-       /*dots:true,
-       fade: true*/
-
+    
+    
    });
 
    $('body').flipLightBox
