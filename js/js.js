12a94eeedd7d718fab77d25d3d87b8103640699b
andrew levun <andrewlevun@gmail.com>
2019-11-29 16:38:11 +0300

    up_work_7

--- js/js.js
+++ js/js.js
@@ -1,28 +1,12 @@
 
-   $('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 1
-       /*centerMode: true,
-       ceterPadding: "25px",
-       /*dots:true,
-       fade: true*/
-
-   });
-
-   $('body').flipLightBox
+  
+   /*$('body').flipLightBox
    ({ lightbox_flip_speed: 500, 
       lightbox_border_color: '#666666' 
-   })
+   })*/
 
    $(document).ready(function(){
       $('.nivo-lightbox a').nivoLightbox({ effect: 'fade' });
       });
   
-   $(document).ready(function() {
-      $('#myCarousel').carousel({
-      interval: 10000
-      })
-      });
-
    
